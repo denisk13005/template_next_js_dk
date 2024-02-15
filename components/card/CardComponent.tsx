f3d1269cfebbee26ef20aca070cece8e55328674
denisk13005 <kirmanndenis@gmail.com>
2024-02-15 17:51:12 +0100

    test card nouveau design

--- components/card/CardComponent.tsx
+++ components/card/CardComponent.tsx
@@ -1,17 +1,24 @@
 "use client";
-import Link from "next/link";
+import { useRouter } from "next/navigation";
 import styles from "./styles.module.scss";
 
 export default function CardComponent() {
+  const router = useRouter();
+  const redirectToStripe = async () => {
+    router.push("https://buy.stripe.com/test_7sI15daPAf2tabucMM");
+  };
   return (
     <article className={styles.cardContainer}>
       <h3>title</h3>
       <section>informations</section>{" "}
-      <Link href={"https://buy.stripe.com/test_7sI15daPAf2tabucMM"}>
-        <button type="submit" value={10}>
-          10€
-        </button>
-      </Link>
+      <button
+        onClick={redirectToStripe}
+        className={styles.btn}
+        type="submit"
+        value={10}
+      >
+        Buy For Only 10€
+      </button>
     </article>
   );
 }
