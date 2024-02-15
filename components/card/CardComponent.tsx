a1ff6fedb3f5105b86081d02e6f9502f5f16844c
denisk13005 <kirmanndenis@gmail.com>
2024-02-15 17:58:44 +0100

    nouveau lien stripe

--- components/card/CardComponent.tsx
+++ components/card/CardComponent.tsx
@@ -5,19 +5,19 @@ import styles from "./styles.module.scss";
 export default function CardComponent() {
   const router = useRouter();
   const redirectToStripe = async () => {
-    router.push("https://buy.stripe.com/test_7sI15daPAf2tabucMM");
+    router.push("https://buy.stripe.com/test_8wMbJR0aW9I9gzS9AB");
   };
   return (
     <article className={styles.cardContainer}>
-      <h3>title</h3>
-      <section>informations</section>{" "}
+      <h3>Site Vitrine</h3>
+      <section>Réalisation d'un site vitrine clé en main </section>{" "}
       <button
         onClick={redirectToStripe}
         className={styles.btn}
         type="submit"
         value={10}
       >
-        Buy For Only 10€
+        A Partir De 1500€
       </button>
     </article>
   );
